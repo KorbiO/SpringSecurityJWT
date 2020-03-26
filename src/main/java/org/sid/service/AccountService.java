d4ee67dcf397856befe14de0b33479d47e9e9ff5
omarkorbi <46576861+omarkorbi@users.noreply.github.com>
2020-03-26 13:52:39 +0100

    Update AccountService.java

--- src/main/java/org/sid/service/AccountService.java
+++ src/main/java/org/sid/service/AccountService.java
@@ -4,7 +4,8 @@ import org.sid.entities.AppRole;
 import org.sid.entities.AppUser;
 
 public interface AccountService {
-    public AppUser saveUser(String username,String password,String confirmedPassword);
+      public AppUser saveUser(String username,String password,String confirmedPassword,String phoneNumber , String gender, String address );
+    public AppUser saveFournisseur(String username,String password,String confirmedPassword,String phoneNumber , String gender, String address );
     public AppRole save(AppRole role);
     public AppUser loadUserByUsername(String username);
     public void addRoleToUser(String username,String rolename);
